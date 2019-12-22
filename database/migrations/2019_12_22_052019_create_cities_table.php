8527577a637541621ae8cc30797d8c95fe6f07d3
oscargomez87 <oscarstiven87@gmail.com>
2019-12-22 00:21:48 -0500

    Added migration for table cities

---
+++ database/migrations/2019_12_22_052019_create_cities_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCitiesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('cities', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('cities', function (Blueprint $table) {
+            //
+        });
+    }
+}
