4775508ea21a40016acbee3540a6c06152e1046d
oscargomez87 <oscarstiven87@gmail.com>
2019-12-22 00:26:18 -0500

    Added migration for locals

---
+++ database/migrations/2019_12_22_052518_create_locals_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLocalsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('locals', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('locals', function (Blueprint $table) {
+            //
+        });
+    }
+}
